6284990f77852bab7d03bafe2af3319c8dab737b
Ainias <silasg@web.de>
2016-12-04 22:16:09 +0100

    Navigationslink entfernt

--- config/navigation.config.php
+++ config/navigation.config.php
@@ -5,11 +5,11 @@ namespace Ainias\CalDav;
 return array(
     'navigation' => array(
         'left' => array(
-            array(
-                'label' => 'CalDav',
-                'resource' => 'default',
-                'route' => 'calDav',
-            )
+//            array(
+//                'label' => 'CalDav',
+//                'resource' => 'default',
+//                'route' => 'calDav',
+//            )
         ),
     ),
 );
